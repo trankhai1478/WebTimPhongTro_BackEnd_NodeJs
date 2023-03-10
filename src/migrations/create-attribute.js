0479eea26a744edfbff52d69a978bd5d61c819cf
Khai Tran <trankhai1478@gmail.com>
2023-03-10 14:07:55 +0700

    spacedata

--- src/migrations/create-attribute.js
+++ src/migrations/create-attribute.js
@@ -11,7 +11,7 @@ module.exports = {
             price: {
                 type: Sequelize.STRING
             },
-            sacreagetar: {
+            acreage: {
                 type: Sequelize.STRING,
 
             },
